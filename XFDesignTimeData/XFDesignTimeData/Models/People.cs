c4e58b626b2b7c9f6542deeef7270d70bad63381
shuheydev <shuhey.dev@gmail.com>
2019-10-25 17:58:46 +0900

    Add project files.

---
+++ XFDesignTimeData/XFDesignTimeData/Models/People.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace XFDesignTimeData.Models
+{
+    public class People
+    {
+        public string Name { get; set; }
+        public string Location { get; set; }
+        public string Image { get; set; }
+    }
+}
